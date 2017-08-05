ddf0759c0a1a48e62a0242f275bc0e6f3752e9f6
Romserg <serg.romankov@gmail.com>
2017-08-05 11:21:11 +0300

    complete accordion list

--- src/components/accordion-list/accordion-list.js
+++ src/components/accordion-list/accordion-list.js
@@ -10,11 +10,25 @@ export default {
 	methods: {
 		expand(element) {
 			if (element === 0) {
-				this.expandedItems = [true, false, false];
-			} else if (element === 1) {
-				this.expandedItems = [false, true, false];
-			} else if (element === 2) {
-				this.expandedItems = [false, false, true];
+				if (this.expandedItems[0] === true) {
+					this.expandedItems.splice(0, 3, false, false, false);
+				} else {
+					this.expandedItems.splice(0, 3, true, false, false);
+				}
+			}
+			if (element === 1) {
+				if (this.expandedItems[1] === true) {
+					this.expandedItems.splice(0, 3, false, false, false);
+				} else {
+					this.expandedItems.splice(0, 3, false, true, false);
+				}
+			}
+			if (element === 2) {
+				if (this.expandedItems[2] === true) {
+					this.expandedItems.splice(0, 3, false, false, false);
+				} else {
+					this.expandedItems.splice(0, 3, false, false, true);
+				}
 			}
 		}
 	}
